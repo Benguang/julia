c5f9c831fc9eb440dab73c8bcea224dbc50f72ee
Kristoffer Carlsson <kcarlsson89@gmail.com>
2018-07-26 09:59:07 +0200

    use a Pipe instead of a file

--- contrib/generate_precompile.jl
+++ contrib/generate_precompile.jl
@@ -20,7 +20,6 @@ function generate_precompile_statements()
     t = time()
     println("Generating precompile statements...")
     println("──────────────────────────────────────")
-    tmpd = mktempdir()
 
 	# Reset code loading vars
 	Base.ACTIVE_PROJECT[] = nothing
@@ -29,13 +28,15 @@ function generate_precompile_statements()
 	empty!(DEPOT_PATH)
 	push!(LOAD_PATH, "@stdlib")
 
-    push!(DEPOT_PATH, tmpd)
-    push!(LOAD_PATH, tmpd)
-    pkgname = "__PackagePrecompilationStatementModule"
-    generate_precompilable_package(tmpd, pkgname)
-    @eval using __PackagePrecompilationStatementModule
-    pop!(LOAD_PATH)
-    pop!(DEPOT_PATH)
+    mktempdir() do tmpd
+        push!(DEPOT_PATH, tmpd)
+        push!(LOAD_PATH, tmpd)
+        pkgname = "__PackagePrecompilationStatementModule"
+        generate_precompilable_package(tmpd, pkgname)
+        @eval using __PackagePrecompilationStatementModule
+        pop!(LOAD_PATH)
+        pop!(DEPOT_PATH)
+    end
 
     if isempty(ARGS)
         sysimg = joinpath(dirname(Sys.BINDIR), "lib", "julia", "sys.ji")
@@ -43,8 +44,8 @@ function generate_precompile_statements()
         sysimg = ARGS[1]
     end
 
-    tmp = tempname()
-    touch(tmp)
+    output = Pipe()
+    task = @async read(output, String)
     have_repl =  haskey(Base.loaded_modules,
                         Base.PkgId(Base.UUID("3fa0cd96-eef1-5676-8a61-b3b8758bbffb"), "REPL"))
     if have_repl
@@ -60,23 +61,34 @@ function generate_precompile_statements()
         """
         # Do not redirect stdin unless it is to a tty, because that changes code paths
         run(pipeline(`$(julia_cmd()) --sysimage $sysimg --trace-compile=yes -O0
-                     --startup-file=no --q -e $setup -i`; stderr=tmp))
+                     --startup-file=no --q -e $setup -i`; stderr=output))
     else
         # No REPL, just record the startup
         run(pipeline(`$(julia_cmd()) --sysimage $sysimg --trace-compile=yes -O0
-                     --startup-file=no --q -e0`; stderr=tmp))
+                     --startup-file=no --q -e0`; stderr=output))
     end
 
     # Replace the FakeTerminal with a TTYTerminal and filter out everything we compiled in Main
-    precompiles = readlines(tmp)
+    close(output)
+    precompiles = split(fetch(task), '\n')
     new_precompiles = Set{String}()
     for statement in precompiles
         startswith(statement, "precompile(Tuple{") || continue
         statement = replace(statement, "FakeTerminals.FakeTerminal" => "REPL.Terminals.TTYTerminal")
         (occursin(r"Main.", statement) || occursin(r"FakeTerminals.", statement)) && continue
-        push!(new_precompiles, statement)
+        # AppVeyor CI emits a single faulty precompile statement:
+        # precompile(Tuple{getfield(precompile(Tuple{typeof(Base.uvfinalize), Base.PipeEndpoint})
+        # which lacks the correct closing brackets.
+        # Filter out such lines here.
+        for (l, r) in ('(' => ')', '{' => '}')
+            if count(isequal(l), statement) != count(isequal(r), statement)
+                continue
+            end
+            push!(new_precompiles, statement)
+        end
     end
 
+    tmp = tempname()
     write(tmp, join(sort(collect(new_precompiles)), '\n'))
     # Load the precompile statements
     PrecompileStagingArea = Module()
@@ -85,21 +97,23 @@ function generate_precompile_statements()
             eval(PrecompileStagingArea, :($(Symbol(_mod)) = $_mod))
         end
     end
-
     Base.include(PrecompileStagingArea, tmp)
+    rm(tmp)
+
+    # Add a few manual things, run `julia` with `--trace-compile` to find these.
     if have_repl
         @eval PrecompileStagingArea begin
             # Could startup with REPL banner instead but it is a bit visually noisy,
-            # so just precompile it here.
-            @assert precompile(Tuple{typeof(Base.banner), REPL.Terminals.TTYTerminal})
-			# This is important for precompilation but doesn't seem to get caught in the script above
-            @assert precompile(Tuple{typeof(Base.create_expr_cache), String, String, Array{Base.Pair{Base.PkgId, UInt64}, 1}, Base.UUID})
+            # so just precompile it manualally here.
+            precompile(Tuple{typeof(Base.banner), REPL.Terminals.TTYTerminal})
+
+            # This is probablably important for precompilation (0.2s precompile time)
+            # but doesn't seem to get caught in the script above
+            precompile(Tuple{typeof(Base.create_expr_cache), String, String, Array{Base.Pair{Base.PkgId, UInt64}, 1}, Base.UUID})
 
         end
     end
 
-    rm(tmp)
-    rm(tmpd; recursive=true)
     println("──────────────────────────────────────")
     print("$(length(new_precompiles)) precompile statements generated in "), Base.time_print((time() - t) * 10^9)
     println()
